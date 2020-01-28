363260fe3146989c0b9b7399e666daa516348ee9
akashjava <akashkumardutta2@gmail.com>
2020-01-28 14:47:28 +0530

    Updates

---
+++ precache-manifest.a57bb97b0cad1b567b0c80a49d0d50d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f18354557009ae013f4c83bf3259f2",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "a1dedc332fe6b438a751",
+    "url": "/myreact/static/css/main.28b694dc.chunk.css"
+  },
+  {
+    "revision": "a218ce6b0563ea42f7c0",
+    "url": "/myreact/static/js/2.73c2d01e.chunk.js"
+  },
+  {
+    "revision": "a1dedc332fe6b438a751",
+    "url": "/myreact/static/js/main.fa0f2868.chunk.js"
+  },
+  {
+    "revision": "9186904f6f675384e5e6",
+    "url": "/myreact/static/js/runtime-main.e52529c3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/akashjava/myreact/precache-manifest.31765193fdde02a48ac0604cbce7ddaf.js"
+  "/myreact/precache-manifest.a57bb97b0cad1b567b0c80a49d0d50d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/akashjava/myreact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myreact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
